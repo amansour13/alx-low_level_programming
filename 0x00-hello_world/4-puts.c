946ae2741ca74d82041fc51e4a6b9b47dd5a67f3
amansour13 <a.mansour.dev1@gmail.com>
2023-02-17 09:24:01 -0800

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+/*
+ use puts fn to output on the screen
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
