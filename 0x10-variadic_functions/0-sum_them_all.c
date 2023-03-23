e8e4986865c55d6f4aa70a8ff9573bdcc6d85fd7
amansour13 <a.mansour.dev1@gmail.com>
2023-03-23 16:43:59 -0700

    hh

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,11 @@
  *
  * Return: The sum of the integers.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i, sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	va_start(valist, n);
 
